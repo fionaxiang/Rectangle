2464ad9bc9fa22f67b96cd756ab4c41a1383d860
unknown <cl20879@an098064-ct103v.nvstu.edu>
2015-02-19 15:30:26 -0500

    Add a java file

---
+++ Rectangle.java
@@ -0,0 +1,15 @@
+
+public class Rectangle{
+	public static void main(String [] args){
+		double height, width, Area, Perimeter;
+		
+		height = 3.5;
+		width = 5;
+		Area = height * width;
+		Perimeter = 2*(height + width);
+		
+		System.out.println("The rectangle's area is " + Area + "\n" + "The rectangle's perimeter is " + Perimeter);
+	}
+	
+
+}
